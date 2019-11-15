25c321339a07c47a2f0c852118a8e5919b7e0c64
philemonmastewal <philemonmastewal@gmail.com>
2019-11-14 23:14:12 -0500

    fixed queries

--- src/components/common/Card/index.js
+++ src/components/common/Card/index.js
@@ -4,5 +4,5 @@ export const Card = styled.div`
 	padding: 1rem;
 	background: #fff;
 	height: 100%;
-	box-shadow: 5px 10px 18px red;
+	/* box-shadow: 0 7px 30px -10px rgba(150, 170, 180, 0.5); */
 `
